5cc21433c3b6a1d8c4b0a5a34b434a06a3c18ec1
abdul-barii <abdulbari3088@gmail.com>
2024-11-11 15:12:25 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # Mapping of Roman numerals to their integer values
+        
         roman_values = {
             'I': 1, 'V': 5, 'X': 10, 'L': 50,
             'C': 100, 'D': 500, 'M': 1000
@@ -13,17 +13,17 @@ class Solution(object):
         total = 0
         prev_value = 0
 
-        # Traverse the string from right to left
+        
         for char in reversed(s):
             current_value = roman_values[char]
 
-            # If the current value is less than the previous value, subtract it
+            
             if current_value < prev_value:
                 total -= current_value
             else:
                 total += current_value
 
-            # Update the previous value for the next iteration
+            
             prev_value = current_value
 
         return total
